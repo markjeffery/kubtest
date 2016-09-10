e84f1fe90f016b2b919881cfad1e4475042a083d
Mark Jeffery <mark@jeffery.com>
2016-09-10 23:59:51 +0100

    made changes to server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!');
+  response.end('Hello and welcome to my World!');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
