6fb6f94e23d2b39868d3e07cf9c103f00642b98d
Mark Jeffery <mark@jeffery.com>
2016-09-25 15:27:35 +0100

    Update server.js
    Added many environment variables, which can be controller when starting via docker or kubernetes:
    
    var appdControllerHostname = process.env.APPDYNAMICS_CONTROLLER_HOST_NAME;
    var appdControllerPort = process.env.APPDYNAMICS_CONTROLLER_PORT;
    var appdAccountName = process.env.APPDYNAMICS_AGENT_ACCOUNT_NAME;
    var appdAccountAccessKey = process.env.APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY;
    var appdAgentApplicationName = process.env.APPDYNAMICS_AGENT_APPLICATION_NAME;
    var appdAgentTierName = process.env.APPDYNAMICS_AGENT_TIER_NAME;
    var externalWebService = process.env.EXTERNAL_WEBSERVICE;

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 var os = require("os");
 var hostname = os.hostname();
+var appdControllerHostname = process.env.APPDYNAMICS_CONTROLLER_HOST_NAME;
+var appdControllerPort = process.env.APPDYNAMICS_CONTROLLER_PORT;
+var appdAccountName = process.env.APPDYNAMICS_AGENT_ACCOUNT_NAME;
+var appdAccountAccessKey = process.env.APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY;
+var appdAgentApplicationName = process.env.APPDYNAMICS_AGENT_APPLICATION_NAME;
+var appdAgentTierName = process.env.APPDYNAMICS_AGENT_TIER_NAME;
+var externalWebService = process.env.EXTERNAL_WEBSERVICE;
+var mode   = process.env.NODE_ENV;
 require("appdynamics").profile({
-  controllerHostName: '10.0.0.1',
-  controllerPort: 8090, 
-  accountName: 'customer1',
-  accountAccessKey: '8d59389c-2bb9-4fc1-b495-90016f453d8c',
-  applicationName: 'kubtest',
-  tierName: 'node', 
+  controllerHostName: appdControllerHostname,
+  controllerPort: appdControllerPort, 
+  accountName: appdAccountName,
+  accountAccessKey: appdAccountAccessKey,
+  applicationName: appdAgentApplicationName,
+  tierName: appdAgentTierName, 
   nodeName: hostname, 
  });
 var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello and welcome to my World!');
+  response.end('Hello and welcome to my World!, calling ' + externalWebService);
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
